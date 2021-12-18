95409a4cc86ea0de6a0d7eeca92992ade26a52ff
Abdelrahman Mohamed <80596612+AbdElrahmanBEBO@users.noreply.github.com>
2021-12-18 02:12:55 +0200

    Initial commit

---
+++ src/pacman/AnimListener.java
@@ -0,0 +1,10 @@
+package pacman;
+
+import javax.media.opengl.GLAutoDrawable;
+import javax.media.opengl.GLEventListener;
+import java.awt.event.KeyListener;
+
+public abstract class AnimListener implements GLEventListener , KeyListener {
+    protected String assetsFolderName = "Assets/";
+}
+
---
+++ src/pacman/PACMAN.java
@@ -0,0 +1,31 @@
+package pacman;
+
+import com.sun.opengl.util.*;
+import java.awt.*;
+import javax.media.opengl.*;
+import javax.swing.*;
+
+public class PACMAN extends JFrame {
+    GLCanvas glcanvas = new GLCanvas();
+    Animator animator = new FPSAnimator(30);
+    AnimListener listener = new AnimGLEventListener();
+
+    public static void main(String[] args) {
+        new PACMAN().animator.start();
+    }
+
+    public PACMAN() {
+        glcanvas.addGLEventListener(listener);
+        glcanvas.addKeyListener(listener);
+        animator.add(glcanvas);
+        getContentPane().add(glcanvas, BorderLayout.CENTER);
+
+        setTitle("PAC-MAN");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(850, 850);
+        setLocationRelativeTo(null);
+        setVisible(true);
+        setFocusable(true);
+        glcanvas.requestFocus();
+    }
+}
